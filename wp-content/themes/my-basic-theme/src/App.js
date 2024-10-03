3e9a258f3bc0a5da6c1446f8fe0d241a8c2cbd06
Milly Held <hallo@millyheld.nl>
2024-10-03 14:27:20 +0200

    Add basic theme set up with React

---
+++ wp-content/themes/my-basic-theme/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from '@wordpress/element';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, WordPress and React!</h1>
+        {/* Your React components will go here */}
+      </div>
+		);
+    }
+}
